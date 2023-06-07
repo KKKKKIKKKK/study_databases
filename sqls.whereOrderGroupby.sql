053bb4c2619d0ff35843312d2ffac38147c2d136
Your Name <you@example.com>
2023-06-07 17:42:26 +0900

    count 14.6

--- sqls.whereOrderGroupby.sql
+++ sqls.whereOrderGroupby.sql
@@ -1,7 +1,7 @@
-SELECT COUNT(*),EmployeeID
-FROM Orders
-WHERE EmployeeID IN (7 , 9)  -- 1)조건 고용 아이디가 7.9 직원에 주문 갯수 .
-GROUP BY EmployeeID           -- 2)그룹화 
+SELECT * FROM Orders
+WHERE EmployeeID IN (7,9)
+GROUP BY EmployeeID   
 
 -- Number of Records: 2
 -- refer : https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_orderby
+-- count 14 , 6
