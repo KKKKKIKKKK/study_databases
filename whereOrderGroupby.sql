60c1feefb6dd5eeade0c805176cae653c5b3dd0e
Your Name <you@example.com>
2023-06-07 17:39:29 +0900

    WHERE ORDER

---
+++ whereOrderGroupby.sql
@@ -0,0 +1,6 @@
+SELECT * FROM Orders
+WHERE EmployeeID IN (7,9)
+GROUP BY EmployeeID   
+
+-- Number of Records: 2
+-- refer : https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_orderby
