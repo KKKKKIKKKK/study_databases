f069d2b3857fb176d49cad2814dd88a63b540c6a
Your Name <you@example.com>
2023-06-07 12:32:10 +0900

    퀘스트

---
+++ Memers_Deletes.sql
@@ -0,0 +1,16 @@
+-- DELETE FROM table_name WHERE condition;
+
+
+
+DELETE FROM members_hobby ;  
+
+DELETE FROM membewrs_login ;
+
+DELETE FROM hobby_code ;
+
+DELETE FROM members ;
+
+DELETE FROM gender_code ;
+
+DELETE FROM address_code ;
+
