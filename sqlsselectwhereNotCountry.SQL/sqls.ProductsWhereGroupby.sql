2f4676249264a064c8123b1ec963070e87a5bbdf
Your Name <you@example.com>
2023-06-08 11:06:39 +0900

    결과값 재업로드

--- sqlsselectwhereNotCountry.SQL/sqls.ProductsWhereGroupby.sql
+++ sqlsselectwhereNotCountry.SQL/sqls.ProductsWhereGroupby.sql
@@ -1,7 +1,19 @@
 -- refer: https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_having
 -- Result Number of Records: 5
 
+
+
 SELECT CategoryID, COUNT(CategoryID) AS CNT_CategoryID 
 FROM Products AS T_PRODUCT
 GROUP BY CategoryID
 HAVING COUNT(CategoryID) >= 10;
+
+-- Result
+-- CategoryID	CNT_CategoryID
+--  1	         12
+--  2	         12
+--  3	         13
+--  4	         10
+--  8	         12
+
+
